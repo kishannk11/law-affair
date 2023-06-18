030cd63f6247cf7000e0974f426d7b9142ebd6ad
6Ro0T <ranjithchandran220@gmail.com>
2023-06-18 23:31:04 +0530

    made updates

--- admin/add_client.php
+++ admin/add_client.php
@@ -6,20 +6,25 @@ require_once 'client_file.php';
     $name = $client->sanitizeInput($_POST['name']);
     $mobileNumber = $client->sanitizeInput($_POST['mobileNumber']);
     $address = $client->sanitizeInput($_POST['address']);
+    $username = 'CLNT' . rand(1000, 9999);
      if ($client->validateName($name) && $client->validateMobileNumber($mobileNumber) && $client->validateAddress($address)) {
         $photo = 'uploads/' . basename($_FILES['photo']['name']);
         $document = 'uploads/' . basename($_FILES['document']['name']);
          if ($client->uploadFile($_FILES['photo'], $photo) && $client->uploadFile($_FILES['document'], $document)) {
-            if ($client->addClient($name, $mobileNumber, $photo, $address, $document)) {
-                echo "Client added successfully.";
+            if ($client->addClient($name, $mobileNumber, $photo, $address, $document,$username)) {
+                 $success="Client added successfully.";
+                 header("Location: client.php?success=".urlencode($success));
             } else {
-                echo "Error: Could not add client.";
+                $error="Error: Could not add client.";
+                header("Location: client.php?error=".urlencode($error));
             }
         } else {
-            echo "Error: Could not upload files.";
+            $error="Error: Could not upload files.";
+            header("Location: client.php?error=".urlencode($error));
         }
     } else {
-        echo "Error: Invalid input.";
+        $error= "All fields required";
+        header("Location: client.php?required=".urlencode($error));
     }
 }
 ?>
